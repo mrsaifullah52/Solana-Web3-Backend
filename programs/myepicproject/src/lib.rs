77a7e01c4190be56b7bec6663e32cf96bed53168
Saif Ullah <mrsaifullah52@gmail.com>
2022-06-09 17:57:45 +0500

    AddGif Fn Added in lib

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -14,6 +14,14 @@ pub mod myepicproject {
         base_account.total_gifs=0;
         Ok(())
     }
+
+
+    pub fn add_gif(ctx: Context<AddGif>) -> Result <()>{
+        let base_account=&mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
@@ -25,6 +33,12 @@ pub struct StartStuffOff<'info> {
     pub system_program: Program <'info, System>,
 }
 
+
+#[derive(Accounts)]
+pub struct AddGif<'info>{
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
 
 #[account]
 pub struct BaseAccount {
