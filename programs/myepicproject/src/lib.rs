e194989f4c1928ddc87c4145499ea9ebd051cefa
Saif Ullah <mrsaifullah52@gmail.com>
2022-06-10 22:52:15 +0500

    programm id replaced

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3FehMEw9HgRbd3rzs8VUgQRXTcQo1i6WfnzJX4QTGgAp");
 
 #[program]
 pub mod myepicproject {
